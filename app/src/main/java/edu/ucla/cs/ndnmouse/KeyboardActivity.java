30973abf17ecff5a3f5d6edf4c2c02fe96c842ee
Wesley Minner <wesleyminner@gmail.com>
2017-06-01 16:23:12 -0700

    Deprecate keyboard activity (was merged into mouse activity a while back).

--- app/src/main/java/edu/ucla/cs/ndnmouse/KeyboardActivity.java
+++ app/src/main/java/edu/ucla/cs/ndnmouse/KeyboardActivity.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 
 import edu.ucla.cs.ndnmouse.utilities.Server;
 
+// Deprecated class
+// activity_keyboard layout merged into activity_mouse layout using a ViewFlipper
 public class KeyboardActivity extends AppCompatActivity {
 
     private static final String TAG = KeyboardActivity.class.getSimpleName();
@@ -23,90 +25,90 @@ public class KeyboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keyboard);
 
-        mKeyboardStatusTextView = (TextView) findViewById(R.id.tv_keyboard_touchpad);
-        setupCallbacks();
+//        mKeyboardStatusTextView = (TextView) findViewById(R.id.tv_keyboard_touchpad);
+//        setupCallbacks();
     }
 
-    /**
-     * Helper function to setup each callback (for buttons and touchpad)
-     */
-    private void setupCallbacks() {
-        // Up Arrow Key
-        final Button upArrowButton = (Button) findViewById(R.id.b_up_arrow);
-        upArrowButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    mServer.executeCommand(R.string.action_keypress_up_arrow_down);
-                    displayKeyPress(getString(R.string.action_keypress_up_arrow_down));
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    mServer.executeCommand(R.string.action_keypress_up_arrow_up);
-                    displayKeyPress(getString(R.string.action_keypress_up_arrow_up));
-                } else {
-                    return false;
-                }
-                return true;
-            }
-        });
-
-        // Down Arrow Key
-        final Button downArrowButton = (Button) findViewById(R.id.b_down_arrow);
-        downArrowButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    mServer.executeCommand(R.string.action_keypress_down_arrow_down);
-                    displayKeyPress(getString(R.string.action_keypress_down_arrow_down));
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    mServer.executeCommand(R.string.action_keypress_down_arrow_up);
-                    displayKeyPress(getString(R.string.action_keypress_down_arrow_up));
-
-                } else {
-                    return false;
-                }
-                return true;
-            }
-        });
-
-        // Left Arrow Key
-        final Button leftArrowButton = (Button) findViewById(R.id.b_left_arrow);
-        leftArrowButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    mServer.executeCommand(R.string.action_keypress_left_arrow_down);
-                    displayKeyPress(getString(R.string.action_keypress_left_arrow_down));
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    mServer.executeCommand(R.string.action_keypress_left_arrow_up);
-                    displayKeyPress(getString(R.string.action_keypress_left_arrow_up));
-
-                } else {
-                    return false;
-                }
-                return true;
-            }
-        });
-
-        // Right Arrow Key
-        final Button rightArrowButton = (Button) findViewById(R.id.b_right_arrow);
-        rightArrowButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    mServer.executeCommand(R.string.action_keypress_right_arrow_down);
-                    displayKeyPress(getString(R.string.action_keypress_right_arrow_down));
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    mServer.executeCommand(R.string.action_keypress_right_arrow_up);
-                    displayKeyPress(getString(R.string.action_keypress_right_arrow_up));
-                } else {
-                    return false;
-                }
-                return true;
-            }
-        });
-    }
-
-    private void displayKeyPress(String keyPress) {
-        mKeyboardStatusTextView.setText(keyPress);
-    }
+//    /**
+//     * Helper function to setup each callback (for buttons and touchpad)
+//     */
+//    private void setupCallbacks() {
+//        // Up Arrow Key
+//        final Button upArrowButton = (Button) findViewById(R.id.b_up_arrow);
+//        upArrowButton.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//                    mServer.executeCommand(R.string.action_keypress_up_arrow_down);
+//                    displayKeyPress(getString(R.string.action_keypress_up_arrow_down));
+//                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+//                    mServer.executeCommand(R.string.action_keypress_up_arrow_up);
+//                    displayKeyPress(getString(R.string.action_keypress_up_arrow_up));
+//                } else {
+//                    return false;
+//                }
+//                return true;
+//            }
+//        });
+//
+//        // Down Arrow Key
+//        final Button downArrowButton = (Button) findViewById(R.id.b_down_arrow);
+//        downArrowButton.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//                    mServer.executeCommand(R.string.action_keypress_down_arrow_down);
+//                    displayKeyPress(getString(R.string.action_keypress_down_arrow_down));
+//                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+//                    mServer.executeCommand(R.string.action_keypress_down_arrow_up);
+//                    displayKeyPress(getString(R.string.action_keypress_down_arrow_up));
+//
+//                } else {
+//                    return false;
+//                }
+//                return true;
+//            }
+//        });
+//
+//        // Left Arrow Key
+//        final Button leftArrowButton = (Button) findViewById(R.id.b_left_arrow);
+//        leftArrowButton.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//                    mServer.executeCommand(R.string.action_keypress_left_arrow_down);
+//                    displayKeyPress(getString(R.string.action_keypress_left_arrow_down));
+//                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+//                    mServer.executeCommand(R.string.action_keypress_left_arrow_up);
+//                    displayKeyPress(getString(R.string.action_keypress_left_arrow_up));
+//
+//                } else {
+//                    return false;
+//                }
+//                return true;
+//            }
+//        });
+//
+//        // Right Arrow Key
+//        final Button rightArrowButton = (Button) findViewById(R.id.b_right_arrow);
+//        rightArrowButton.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//                    mServer.executeCommand(R.string.action_keypress_right_arrow_down);
+//                    displayKeyPress(getString(R.string.action_keypress_right_arrow_down));
+//                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+//                    mServer.executeCommand(R.string.action_keypress_right_arrow_up);
+//                    displayKeyPress(getString(R.string.action_keypress_right_arrow_up));
+//                } else {
+//                    return false;
+//                }
+//                return true;
+//            }
+//        });
+//    }
+//
+//    private void displayKeyPress(String keyPress) {
+//        mKeyboardStatusTextView.setText(keyPress);
+//    }
 }
